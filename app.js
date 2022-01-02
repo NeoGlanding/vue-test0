c71aa3187de3fb38c54a83ac17554ab05a3e4b5b
NeoGlanding <neoglanding@protonmail.com>
2022-01-02 13:58:20 +0700

    function data binding

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ let app = Vue.createApp({
             name: 'Ihsan',
             vue: 'https://v3.vuejs.org'
         }
+    },
+    methods: {
+        brotherName(param) {
+            return `Hello everyone, this is my brother ${param}`
+        }
     }
 });
 
