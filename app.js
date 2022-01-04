a536f9a23be2ececa50fde1c42f9ab216bf5e298
NeoGlanding <neoglanding@protonmail.com>
2022-01-04 12:38:40 +0700

    test vue

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ let app = Vue.createApp({
     data() {
         return {
             name: 'Ihsan',
-            vue: 'https://v3.vuejs.org'
+            vue: 'https://v3.vuejs.org',
+            godOfThunder: 'Thor Son of Odin',
+            h2Element: '<h2>Superboy</h2>',
         }
     },
     methods: {
         brotherName(param) {
             return `Hello everyone, this is my brother ${param}`
+        },
+        vueName() {
+            return this.godOfThunder
         }
     }
 });
